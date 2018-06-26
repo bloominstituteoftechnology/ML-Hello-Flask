cfa53c015ac6306882d12d9a8e38f5bfcd5a1b73
Darwin Johnson <schwaaweb@gmail.com>
2018-06-26 11:17:01 -0400

    initial Pipfile

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# OSX 10.13.5 || Python 3.6.4 :: Anaconda custom (64-bit)
+git clone git@github.com:schwaaweb/ML-Hello-Flask.git
+cd ML-Hello-Flask/
+pip install pipenv
+pipenv --python 3
+
+
